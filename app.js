91d6c03c044ea5f8b9eb08845fd5089356ad159c
forcousteau <forcousteau@gmail.com>
2019-05-08 23:49:11 +0300

    lesson add delete and edit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+const mongoose = require('mongoose');
 
 const app = express();
 
+
+//map global promise - ger rid of warning(i kinda don t have it)
+//mongoose.Promise = global.Promise;
 //Connect to mongoose
 mongoose.connect('mongodb://localhost/notelist-db', {
   useNewUrlParser: true
@@ -13,7 +17,7 @@ mongoose.connect('mongodb://localhost/notelist-db', {
   .catch(err => console.log('DB connect Error'));
 
 //Load Note Model from models/note.js
-require('./models/note');
+require('./models/Note');
 const Note = mongoose.model('notes');
 
 //Handlebars middleware
@@ -21,12 +25,13 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
-//body parser middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//methodoverrride middleware
+app.use(methodOverride('_method'));
 
 //index route
 app.get('/', (req, res) => {
@@ -39,12 +44,41 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render("about");
 });
+//Note index page
+app.get('/notes', (req, res) => {
+  Note.find({})
+    .sort({ date: 'desc' })
+    .then(notes => {
+      res.render('notes/index', {
+        notes: notes
+      });
+    });
+});
 
 //add note form /notes/add
 app.get('/notes/add', (req, res) => {
   res.render("notes/add");
 });
 
+//edit note form /notes/add
+app.get('/notes/edit/:id', (req, res) => {
+  Note.findOne({
+    _id: req.params.id
+  })
+    .then(note => {
+      res.render('notes/edit', {
+        note: note
+      });
+    });
+});
+//delete note
+app.delete('/notes/:id', (req, res) => {
+  Note.remove({
+    _id: req.params.id})
+    .then(() =>{
+      res.redirect('/notes');
+    });
+});
 //Process form
 app.post('/notes', (req, res) => {
   let errors = [];
@@ -62,8 +96,33 @@ app.post('/notes', (req, res) => {
       body: req.body.body
     });
   } else {
-    res.send('ok');
+    const newUser = {
+      title: req.body.title,
+      body: req.body.body
+    }
+    new Note(newUser)
+      .save()
+      .then(note => {
+        res.redirect('/notes');
+      });
   }
+});
+
+//edit form process TRY to do this with ajax
+app.put('/notes/:id', (req, res) => {
+  Note.findOne({
+    _id: req.params.id
+  })
+    .then(note => {
+      //new values
+      note.title = req.body.title;
+      note.body = req.body.body;
+
+      note.save()
+      .then(note=>{
+        res.redirect('/notes');
+      })
+    });
 });
 
 const port = 5000;
